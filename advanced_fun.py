6e35a8d8498b0b55acd877ded3c08c372faa4126
Alberto813 <albertomontiel813@gmail.com>
2021-12-01 03:00:19 +0000

    aadiendo funciones avanzadas con funciones lambda

---
+++ advanced_fun.py
@@ -0,0 +1,9 @@
+def run():
+	my_list = [1,2,3,4,5]
+	cube = [i**2 for i in my_list]
+
+	print(cube)
+
+
+if __name__ == "__main__":
+	run()
---
+++ raiz_cuadrada.py
@@ -0,0 +1,8 @@
+def run():
+
+	raices = {i: i**.5 for i in range(1,1001)}
+	print(float(round(raices,2)))
+
+
+if __name__ == "__main__":
+	run()
